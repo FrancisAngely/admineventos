e332ab948b3b45fac5e914b92a998003089dcda4
francis <francis.a.gamboag@gmail.com>
2024-11-07 20:43:16 +0100

    first commit

---
+++ head.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en" data-bs-theme="light" data-menu-color="light" data-topbar-color="dark">
+
+<head>
+    <meta charset="utf-8" />
+    <title>Dashboard | Scoxe - Responsive Bootstrap 5 Admin Dashboard</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta content="A fully featured admin theme which can be used to build CRM, CMS, etc." name="description" />
+    <meta content="Myra Studio" name="author" />
+
+    <!-- App favicon -->
+    <link rel="shortcut icon" href="assets/images/favicon.ico">
+
+    <link href="assets/libs/morris.js/morris.css" rel="stylesheet" type="text/css" />
+
+    <!-- App css -->
+    <link href="assets/css/style.min.css" rel="stylesheet" type="text/css">
+    <link href="assets/css/icons.min.css" rel="stylesheet" type="text/css">
+    <script src="assets/js/config.js"></script>
+</head>
+
+</html>
---
+++ scripts.php
@@ -0,0 +1,15 @@
+<script src="bootstrap-5.3.3-dist/js/bootstrap.bundle.min.js" ></script>
+
+    <script src="https://cdn.jsdelivr.net/npm/chart.js@4.3.2/dist/chart.umd.js" integrity="sha384-eI7PSr3L1XLISH8JdDII5YN/njoSsxfbrkCTnJrzXt+ENP5MOVBxD+l6sEG4zoLp" crossorigin="anonymous"></script><script src="dashboard.js"></script>
+
+
+<script>
+$( document ).ready(function() {
+   
+   $(".select2").select2();
+   
+    
+    
+});      
+      
+</script>  
